70fb9cf9764db7afa8239820c666b7c519dd9b1c
poli8512 <poli8512@gmail.com>
2021-04-04 20:38:10 +0300

    refactor: remove tag type according to rule 2.4 in html-and-css.md

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var taskInput=document.getElementById("new-task");//Add a new task.
 var addButton=document.getElementsByTagName("button")[0];//first button
 var incompleteTaskHolder=document.getElementById("incomplete-tasks");//ul of #incomplete-tasks
 var completedTasksHolder=document.getElementById("completed-tasks");//completed-tasks
-
+console.log(taskInput,addButton,incompleteTaskHolder,completedTasksHolder)
 
 //New task list item
 var createNewTaskElement=function(taskString){
